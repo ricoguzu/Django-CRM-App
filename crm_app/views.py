d2202c2be103affe2760c3db9fc352f89504190d
ricoguzu <ericmuneneguzu@gmail.com>
2023-04-17 23:50:55 +0300

    Registration2

--- crm_app/views.py
+++ crm_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
 
 # Create your views here.
 def home(request):
@@ -28,4 +29,17 @@ def logout_user(request):
     return redirect('home')
 
 def register_user(request):
-    return render(request, 'register.html', {})
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #authenticate and login and redirect to homepage
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You Have Successfully Registered! Welcome")
+            return redirect("home")
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form':form})
