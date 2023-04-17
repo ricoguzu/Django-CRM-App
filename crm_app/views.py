7cbb5e3c84cbea8db10923dbdfdba4386f21a544
ricoguzu <ericmuneneguzu@gmail.com>
2023-04-17 23:20:03 +0300

    Registration

--- crm_app/views.py
+++ crm_app/views.py
@@ -26,3 +26,6 @@ def logout_user(request):
     logout(request)
     messages.success(request, "You Have Been Logged Out....")
     return redirect('home')
+
+def register_user(request):
+    return render(request, 'register.html', {})
